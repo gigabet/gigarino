926e04dff5ac3e9aed7ea066541d8e524e69e481
gigabet <47139205+gigabet@users.noreply.github.com>
2026-07-10 17:56:12 +0300

    WIP

--- app/sport/[[...slug]]/page.tsx
+++ app/sport/[[...slug]]/page.tsx
@@ -1,8 +1,13 @@
+// import { Suspense } from 'react'
+// import { graphql } from 'relay-runtime'
+// import type { PrematchQuery } from '@/app/sport/[[...slug]]/__generated__/PrematchQuery.graphql'
 import PrematchList from '@/app/sport/[[...slug]]/prematch-list'
 import Carousel from '@/app/sport/carousel'
 import ShortcutRow from '@/app/sport/shortcut-row'
 import Sidebar from '@/app/sport/sidebar'
 import Betslip from '@/components/betslip'
+
+// import { getServerEnvironment } from '@/relay/environment.server'
 
 interface PageProps {
   params: {
@@ -14,6 +19,16 @@ interface PageProps {
 export default async function SportPage(props: PageProps) {
   const [_filter = 'all', _sport = null, _league = null] = props.params.slug ?? []
 
+  // const serverEnv = getServerEnvironment()
+  // const queryRef = serverEnv.serverPreloadQuery<PrematchQuery>(
+  //   graphql`
+  //     query PrematchQuery {
+  //       ...PrematchList
+  //     }
+  //   `,
+  //   {}
+  // )
+
   return (
     <main className='z-1 mx-auto grid min-h-screen w-full max-w-480 grid-cols-[16rem_minmax(auto,1fr)_18.75rem] gap-8 px-4 py-6 pb-24 sm:px-6 lg:px-8'>
       {/* <PromotionalBanner /> */}
@@ -22,6 +37,9 @@ export default async function SportPage(props: PageProps) {
         <Carousel />
         <ShortcutRow />
         <PrematchList />
+        {/* <Suspense fallback={'Loading...'}>
+          <PrematchList queryRef={queryRef} />
+        </Suspense> */}
       </section>
       <Betslip />
     </main>
--- app/sport/[[...slug]]/tournament.tsx
+++ app/sport/[[...slug]]/tournament.tsx
@@ -1,17 +1,50 @@
+'use client'
+
 import { GiSoccerBall } from 'react-icons/gi'
+// import { graphql, useFragment } from 'react-relay'
+// import type { TournamentFragment$key } from '@/app/sport/[[...slug]]/__generated__/TournamentFragment.graphql'
 import PrematchEvent from '@/app/sport/[[...slug]]/prematch-event'
 
 export default function Tournament() {
+  // props: { queryRef: TournamentFragment$key }
+  // const data = useFragment(
+  //   graphql`
+  //     fragment TournamentFragment on Tournament {
+  //       sport {
+  //         key
+  //       }
+  //       category {
+  //         countryCode
+  //       }
+  //       name
+  //       events(first: 10) {
+  //         edges {
+  //           node {
+  //             id
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `,
+  //   props.queryRef
+  // )
+
   return (
     <section>
       <h2 className='text-secondary mb-4 flex items-center gap-2 border-b py-2 text-sm'>
         <GiSoccerBall className='size-4.5' />
+        {/* <pre>{data.sport.key}</pre> */}
         <span className='text-uppercase flex size-5 items-center justify-center rounded-full bg-white/3 text-[0.5rem] leading-none'>
           EN
+          {/* {data.category.countryCode} */}
         </span>
-        Premier league
+        {/* {data.name} */}
+        Premier League
       </h2>
       <div className='flex flex-col gap-3'>
+        {/* {data.events.edges.map(edge => (
+          <PrematchEvent key={edge.node.id} />
+        ))} */}
         <PrematchEvent />
         <PrematchEvent />
         <PrematchEvent />
